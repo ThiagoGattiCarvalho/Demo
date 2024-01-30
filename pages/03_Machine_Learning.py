ec55ac0010df4968e0a8b6ed9900eb564c53c538
Thiago <thiagogatti@gmail.com>
2024-01-30 17:08:08 -0300

    Delete pages/03_Machine_Learning.py

--- pages/03_Machine_Learning.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(layout="wide")
-
-st.title("Machine Learning")
-
-st.markdown("You don't have access to this module. Please, contact Thiago.")
-
-st.markdown("You have access to the Filter and the Analytics modules.")
