8cb45d4528b757f6b242505c217425477fc5435d
Thiago <thiagogatti@gmail.com>
2024-08-07 22:23:58 -0300

    Delete pages directory

--- pages/01_Data_Preparation.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(layout="wide")
-
-st.title("Data Preparation")
-
-st.markdown("You don't have access to this module. Please, contact Thiago.")
--- pages/03_Machine_Learning.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(layout="wide")
-
-st.title("Machine Learning")
-
-st.markdown("You don't have access to this module. Please, contact Thiago.")
